6c72822e70f8f8af8ceea1f1c10d2ee71b134061
TD2-UNLaM <43824155+TD2-UNLaM@users.noreply.github.com>
2022-09-05 19:46:23 -0300

    first commit

---
+++ PA2-2022-2C-WebAppMVC/Models/Provincias.cs
@@ -0,0 +1,8 @@
+﻿namespace PA2_2022_2C_WebAppMVC.Models
+{
+    public class Provincias
+    {
+        public int Provincia { get; set; }
+        public string? NomProvincia { get; set; }
+    }
+}
